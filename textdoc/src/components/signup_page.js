8996f5a87250fd34e39083a228daac894c460e27
Philip <pkim90621@gmail.com>
2023-05-20 18:02:44 -0700

    Created css for login page and sign up page. Configured firebase authentication with the login page but signup page still needs to be worked on to properly create a new user account.

---
+++ textdoc/src/components/signup_page.js
@@ -0,0 +1,61 @@
+import React, {useState} from "react";
+import "./login-signup.css"
+import { auth, app } from "../firebaseConfig"
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+export default function SignUp() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  }
+
+  const signUp = (event) => {
+    event.preventDefault();
+
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+
+    console.log(userCredential);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+}
+
+  return (
+    <>
+      <div className="vertical-container">
+      <section class="box-container">
+      <div class="heading">
+        <h1 class="text text-large">Create an Account</h1>
+      </div>
+      <form onSubmit={signUp}>
+        <div class="input-container">
+          <input type="email" placeholder="Enter your email" class="input-field" value={email} onChange={handleEmail}>
+          </input>
+        </div>
+
+        <div class="input-container">
+          <input type="password" placeholder="Enter your password" class="input-field" value={password} onChange={handlePassword}>
+          </input>
+        </div>
+
+        <p class="text text-normal">Already a user? <span><a href="/login">Sign In</a></span>
+        </p>
+
+        <div>
+          <button type="submit" name="submit" class="submit-button" value="Sign In">Submit</button>
+        </div>
+      </form>
+      </section>
+
+      </div>
+    </>
+  );
+}
